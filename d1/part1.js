78528029970c067aaeb67283826bf23b7b2b6ae1
unknown <rayalpj@yahoo.com>
2025-12-02 21:13:44 -0500

    add comments to D1

--- d1/part1.js
+++ d1/part1.js
@@ -5,11 +5,14 @@ const parseLines = async () => {
     return data.split('\n');
 };
 
+// cast number to range 0-99
 const castTo100 = (num, step) => { return ((num + step) % 100 + 100) % 100 };
 
+// sum the times we land on 0
 const solve = async () => {
     const rotations = await parseLines();
 
+    // define left as negative, right as positive
     const steps = rotations.map( r => r.startsWith('L') ? -1 * r.match(/\d+/g) : 1 * r.match(/\d+/g) );
 
     let password = 0;
